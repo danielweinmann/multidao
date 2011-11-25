f90e42c97282098ce7d65efc3577bb0624c85066
Daniel Weinmann <danielweinmann@gmail.com>
2011-11-25 00:21:34 -0200

    First commit

---
+++ main.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'haml'
+require 'sass'
+require 'compass'
+
+configure do
+#  Compass.configuration do |config|
+#    config.project_path = File.dirname(__FILE__)
+#    config.sass_dir = 'views'
+#  end
+#
+  set :haml, { :format => :html5 }
+  set :sass, Compass.sass_engine_options
+end
+
+get '/screen.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :screen
+end
+
+get '/' do
+  haml :index
+end
